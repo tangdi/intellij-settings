e9930eca4fd10bc97348953301ac02fd7e507696
Di Tang <Di.Tang@blackboard.com>
2017-07-13 14:31:17 +0800

    IU-2017.1.5 <dtang@di-tangs-macbook-pro.local Create markdown.xml, IntelliLang.xml, project.default.xml, diff.xml, Default.xml, packages.xml, ignore.xml, find.xml, gradle.run.settings.xml, code.style.schemes.xml, Visual Studio 2015 Dark.icls, path.macros.xml, Darcula copy.icls, github_settings.xml, mavenVersion.xml, Eclectide Monokai.icls, Default.xml, scala.xml, debugger.renderers.xml, Solarized Dark.icls, Blackboard Conventions.xml, editor.codeinsight.xml, Solarized Light.icls, keymap.xml, editor.xml, applicationLibraries.xml, debugger.xml, vim_settings.xml, ide.general.xml, git.xml, other.xml, mbaas-formatter.xml, ui.lnf.xml, gradle.settings.xml, filetypes.xml, projectView.xml, Default copy.icls, Solarized Light (Darcula).icls, plugin_ui.xml, colors.scheme.xml, databaseDrivers.xml, conda_packages.xml, go.sdk.xml, parameter.hints.xml, Class.java, jdk.table.xml, laf.xml, Solarized Dark (Darcula).icls, Mac OS X 10_5_ copy.xml, web-browsers.xml, proxy.settings.xml, vcs.xml, databaseSettings.xml, toStringTemplates.xml, usageView.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) ${YEAR}, Blackboard Inc. All Rights Reserved.
+ */
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+
+/**
+ * ClassName: ${NAME} Function: TODO
+ *
+ * @Author: ${USER}
+ * @Date: ${DATE}, ${TIME}
+ */
+public class ${NAME} {
+}
